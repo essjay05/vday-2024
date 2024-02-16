8f0f792038654020401437338f431c131596fa8b
Joy S <stepjoyous@gmail.com>
2024-02-15 21:58:09 -0800

    Refactor hearts related jsx and css into their own .css and .jsx files for organization.

---
+++ src/components/hearts.jsx
@@ -0,0 +1,13 @@
+import './hearts.css'
+
+export const Hearts = () => {
+  return (
+    <div className="hearts">
+      <div className="heart a1"></div>
+      <div className="heart a2"></div>
+      <div className="heart a3"></div>
+      <div className="heart a4"></div>
+      <div className="heart a5"></div>
+    </div>
+  )
+}
